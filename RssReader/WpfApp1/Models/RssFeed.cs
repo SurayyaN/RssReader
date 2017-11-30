25a94a562059f56141ef5b88c3f985ba5aa8c522
Sue3452313 <s3452313@student.rmit.edu.au>
2017-12-01 10:49:53 +1100

    added RssFeed and RssFeedItem models

---
+++ RssReader/WpfApp1/Models/RssFeed.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel.Syndication;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1.Models
+{
+    public class RssFeed
+    {
+        public SyndicationFeed Feed { get; set; }
+    }
+}
---
+++ RssReader/WpfApp1/Models/RssFeedItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1.Models
+{
+    public class RssFeedItem
+    {
+        public string Website { get; set; }
+        
+        public string Article { get; set; }
+
+        public string Description { get; set; }
+
+        public string PublishedDateTimeOffset { get; set; }
+    }
+}
