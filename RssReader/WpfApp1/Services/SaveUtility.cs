1b7ae5316c2bb95bba9f99ef1b19e0757e9cb7cd
Sue3452313 <s3452313@student.rmit.edu.au>
2017-12-01 23:50:19 +1100

    Added save and loadfromfile utilities

---
+++ RssReader/WpfApp1/Services/SaveUtility.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfApp1.Models;
+using Newtonsoft.Json;
+
+namespace WpfApp1.Services
+{
+    public static class SaveUtility
+    {
+        public static void SaveToFile(ObservableCollection<RssFeedSubscription> rssFeedSubscriptionList)
+        {
+            //using (StreamWriter streamWriter = File.CreateText(@"c:\RSS Reader.json"))
+            //{
+            //    JsonSerializer serializer = new JsonSerializer();
+            //    serializer.Serialize(streamWriter, rssFeedSubscriptionList);
+            //}
+
+            using (StreamWriter streamWriter = File.CreateText(@"c:\RSS Reader.json"))
+            {
+                foreach (RssFeedSubscription subscription in rssFeedSubscriptionList)
+                {
+                    streamWriter.WriteLine(subscription.Feed.Links[0].Uri);
+                }
+            }
+
+            //    string jsonString = JsonConvert.SerializeObject(rssFeedSubscriptionList, Formatting.Indented);
+            //File.WriteAllText(@"c:\RSS Reader.json", jsonString);
+        }
+
+        public static List<string> LoadFromFile()
+        {
+            string filename = @"c:\RSS Reader.json";
+            List<string> feed = null; 
+            
+            if (File.Exists(filename))
+            {
+                string[] feedString = File.ReadAllLines(filename);
+                feed = new List<string>(feedString);
+            }
+
+            return feed;
+        }
+
+        //public static ObservableCollection<RssFeedSubscription> LoadFromFile()
+        //{
+        //    ObservableCollection<RssFeedSubscription> subscriptionsList = null;
+
+        //    //if (File.Exists(@"c:\RSS Reader.json"))
+        //    //{
+        //    //    subscriptionsList = JsonConvert.DeserializeObject<ObservableCollection<RssFeedSubscription>>(File.ReadAllText(@"c:\RSS Reader.json"));
+        //    //}
+
+        //    //using (StreamReader streamReader = File.OpenText(@"c:\RSS Reader.json"))
+        //    //{
+        //    //    JsonSerializer serializer = new JsonSerializer();
+        //    //    subscriptionsList = (ObservableCollection<RssFeedSubscription>) serializer.Deserialize(streamReader,
+        //    //            typeof(ObservableCollection<RssFeedSubscription>));
+        //    //}
+
+        //    return subscriptionsList;
+        //}
+    }
+}
--- RssReader/WpfApp1/Services/SubscriptionsManager.cs
+++ RssReader/WpfApp1/Services/SubscriptionsManager.cs
@@ -2,8 +2,11 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.ServiceModel.Syndication;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
+using Newtonsoft.Json.Schema;
 using WpfApp1.Models;
 
 namespace WpfApp1.Services
@@ -16,6 +19,32 @@ namespace WpfApp1.Services
         public SubscriptionsManager()
         {
             _rssFeedSubscriptionsList = new ObservableCollection<RssFeedSubscription>();
+
+            if (SaveUtility.LoadFromFile() != null)
+            {
+                foreach (string uri in SaveUtility.LoadFromFile())
+                {
+                    Uri uriResult;
+                    bool result = Uri.TryCreate(uri, UriKind.Absolute, out uriResult)
+                                  && (uriResult.Scheme == Uri.UriSchemeHttp || uriResult.Scheme == Uri.UriSchemeHttps);
+                    if (!result)
+                        continue;
+
+                    //XmlReader xmlReader = XmlReader.Create(uri);
+                    //SyndicationFeed feed = SyndicationFeed.Load(xmlReader);
+
+                    XmlReaderSettings settings = new XmlReaderSettings();
+                    settings.DtdProcessing = DtdProcessing.Parse;
+                    //settings.ValidationType = ValidationType.DTD;
+
+                    using (XmlReader reader = XmlReader.Create(uri, settings))
+                    {
+                        SyndicationFeed feed = SyndicationFeed.Load(reader);
+                        AddSubscription(new RssFeedSubscription() {Feed = feed, IsSelected = false});
+                    }
+                }
+                //_rssFeedSubscriptionsList = SaveUtility.LoadFromFile();
+            }
         }
 
         public ObservableCollection<RssFeedSubscription> GetSubscriptions()
@@ -25,11 +54,21 @@ namespace WpfApp1.Services
 
         public void AddSubscription(RssFeedSubscription rssFeed)
         {
-            bool exist = _rssFeedSubscriptionsList.Any(c => c.Feed.Title.Text == rssFeed.Feed.Title.Text);
-            if (!exist)
+            if (_rssFeedSubscriptionsList.Count() != 0)
+            {
+                bool exist = _rssFeedSubscriptionsList.Any(c => c.Feed.Title.Text == rssFeed.Feed.Title.Text);
+                if (!exist)
+                {
+                    _rssFeedSubscriptionsList.Add(rssFeed);
+                }
+            }
+
+            else
             {
                 _rssFeedSubscriptionsList.Add(rssFeed);
             }
+
+            SaveUtility.SaveToFile(_rssFeedSubscriptionsList);
         }
 
         public void RemoveSubscription(RssFeedSubscription rssFeed)
