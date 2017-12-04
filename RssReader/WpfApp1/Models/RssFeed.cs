903f8bc25e5701604f242a3987a1d032b0b87ae5
Sue3452313 <s3452313@student.rmit.edu.au>
2017-12-05 05:08:09 +1100

    commented Models

--- RssReader/WpfApp1/Models/RssFeed.cs
+++ RssReader/WpfApp1/Models/RssFeed.cs
@@ -6,20 +6,50 @@ using System.ServiceModel.Syndication;
 using System.Text;
 using System.Threading.Tasks;
 
+/// <summary>
+/// The Models namespace.
+/// </summary>
 namespace WpfApp1.Models
 {
+    /// <summary>
+    /// Class RssFeed. this model contains the feed and the rss url
+    /// </summary>
+    /// <seealso cref="System.ComponentModel.INotifyPropertyChanged" />
     [Serializable]
     public class RssFeed : INotifyPropertyChanged
     {
+        /// <summary>
+        /// Occurs when a property value changes.
+        /// </summary>
         public event PropertyChangedEventHandler PropertyChanged;
 
+        /// <summary>
+        /// The feed
+        /// </summary>
         private SyndicationFeed _feed;
+
+        /// <summary>
+        /// The is checked. bounded together with checkboxes in the view
+        /// </summary>
         private Boolean _isChecked;
+
+        /// <summary>
+        /// The RSS URL
+        /// </summary>
         private string _rssUrl;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RssFeed"/> class.
+        /// </summary>
         public RssFeed()
         { }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RssFeed"/> class.
+        /// </summary>
+        /// <param name="feed">The feed.</param>
+        /// <param name="rssUrl">The RSS URL.</param>
+        /// <param name="isChecked">The is checked.</param>
         public RssFeed(SyndicationFeed feed, string rssUrl, Boolean isChecked = false)
         {
             _feed = feed;
@@ -27,6 +57,10 @@ namespace WpfApp1.Models
             _rssUrl = rssUrl;
         }
 
+        /// <summary>
+        /// Gets or sets the feed.
+        /// </summary>
+        /// <value>The feed.</value>
         public SyndicationFeed Feed
         {
             get { return _feed; }
@@ -37,6 +71,10 @@ namespace WpfApp1.Models
             }
         }
 
+        /// <summary>
+        /// Gets or sets the is checked.
+        /// </summary>
+        /// <value>The is checked.</value>
         public Boolean IsChecked
         {
             get { return _isChecked; }
@@ -47,6 +85,10 @@ namespace WpfApp1.Models
             }
         }
 
+        /// <summary>
+        /// Gets or sets the RSS URL.
+        /// </summary>
+        /// <value>The RSS URL.</value>
         public string RssUrl
         {
             get { return _rssUrl; }
--- RssReader/WpfApp1/Models/RssFeedItem.cs
+++ RssReader/WpfApp1/Models/RssFeedItem.cs
@@ -7,10 +7,21 @@ using System.Threading.Tasks;
 
 namespace WpfApp1.Models
 {
+    /// <summary>
+    /// Class RssFeedItem.
+    /// </summary>
     public class RssFeedItem
     {
+        /// <summary>
+        /// Gets or sets the channel of the feed
+        /// </summary>
+        /// <value>The website.</value>
         public string Website { get; set; }
 
+        /// <summary>
+        /// Gets or sets the item.
+        /// </summary>
+        /// <value>The item.</value>
         public SyndicationItem Item { get; set; }
     }
 }
