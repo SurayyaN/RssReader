e2fe3a75c54c48803e5fa72d736fa0e6d26413d5
Sue3452313 <s3452313@student.rmit.edu.au>
2017-12-05 12:37:30 +1100

    rename all apfapp1 to rssfeedreader

--- RssReader/WpfApp1/Properties/Resources.Designer.cs
+++ RssReader/WpfApp1/Properties/Resources.Designer.cs
@@ -39,7 +39,7 @@ namespace RssFeedReader.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("WpfApp1.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("RssFeedReader.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
